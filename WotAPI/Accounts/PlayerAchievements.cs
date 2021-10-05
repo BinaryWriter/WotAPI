293d56dfafefe7ec8020c76b402f9ed48c9d6f66
BinaryWriter <obozrevateligrtv@gmail.com>
2021-10-05 22:16:43 +0300

    Fixed summaries in PlayerAchievements.cs

--- WotAPI/Accounts/PlayerAchievements.cs
+++ WotAPI/Accounts/PlayerAchievements.cs
@@ -17,13 +17,13 @@ namespace WotAPI.Accounts
         }
         public partial class AchievementsDictionaries
         {
-            // <summary> RU: Заработанные достижения <para> EN: Achievements earned </para> </summary>
+            /// <summary> RU: Заработанные достижения <para> EN: Achievements earned </para> </summary>
             [JsonProperty("achivements")]
             public Dictionary<string, int> Achievements = new Dictionary<string, int>();
-            // <summary> RU: Прогресс достижений <para> EN: Achievement progress </para> </summary>
+            /// <summary> RU: Прогресс достижений <para> EN: Achievement progress </para> </summary>
             [JsonProperty("frags")]
             public Dictionary<string, int> Frags = new Dictionary<string, int>();
-            // <summary> RU: Максимальные значения серийных достижений <para> EN: Maximum values of achievement series </para> </summary>
+            /// <summary> RU: Максимальные значения серийных достижений <para> EN: Maximum values of achievement series </para> </summary>
             [JsonProperty("max_series")]
             public Dictionary<string, int> MaxSeries = new Dictionary<string, int>();
         }
